b4a43914e601dc3bce84bc98a79a02654683304c
Zah <doanzah2710@gmail.com>
2026-03-14 19:06:48 +0000

    fix(frontend): include openSubmitModal from store to fix vercel build

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -16,6 +16,7 @@ import { useTeamInvites } from '../hooks/useTeamInvites';
 const Navbar = () => {
   const {
     openConnectReminder,
+    openSubmitModal,
     searchQuery,
     setSearchQuery,
     setView,
